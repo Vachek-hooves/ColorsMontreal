db95b4aa6bedbfb96afd4d65547f7557d9babc9a
Vachek-hooves <vachek0602@gmail.com>
2025-02-03 17:22:20 +0200

    predeploy

--- screen/Stack/FactLoading.js
+++ screen/Stack/FactLoading.js
@@ -1,9 +1,16 @@
-import {StyleSheet, Text, View, ActivityIndicator} from 'react-native';
+import {StyleSheet, Text, View, ActivityIndicator, Image} from 'react-native';
+import OrbitCircles from '../../components/ui/OrbitCircles';
 
 const FactLoading = () => {
   return (
     <View style={styles.container}>
-      <View style={styles.OrbitCirclesContainer}></View>
+      <View style={styles.OrbitCirclesContainer}>
+        {/* <OrbitCircles /> */}
+        <Image
+          source={require('../../assets/image/Onboarding/OnboardingLogo.png')}
+          style={styles.image}
+        />
+      </View>
       <Text style={styles.text}>Please wait...</Text>
       <ActivityIndicator size="large" color="#FFA500" style={styles.loader} />
     </View>
@@ -28,6 +35,15 @@ const styles = StyleSheet.create({
     marginTop: '20%',
   },
   loader: {
+    marginTop: 20,
+  },
+  OrbitCirclesContainer: {
+    marginTop: '20%',
+  },
+  image: {
+    width: 200,
+    height: 280,
+    alignSelf: 'center',
     marginTop: 20,
   },
 });
